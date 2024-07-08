526a956d19d50161f59950bdab99c18fe37add07
hos8 <jam39so@outlook.com>
2024-07-09 00:58:12 +0800

    Modify then.cpp

--- then.cpp
+++ then.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# General per letter every agreement hear wrong success.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
