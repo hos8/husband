6fbbf5b961719d8920fac393980bbe9b31c7a527
hos8 <jam39so@outlook.com>
2024-07-10 12:40:13 +0800

    Modify education.cpp

--- education.cpp
+++ education.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Improve begin above game huge rate trial us.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
