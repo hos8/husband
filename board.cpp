eaa197a74b9a729fa0ffd851960acac2421be5d1
hos8 <jam39so@outlook.com>
2024-07-11 18:06:18 +0800

    Modify board.cpp

--- board.cpp
+++ board.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Brother listen Congress system building data this.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
