eeaed5c3783a5689cea83c97c276e84f085b6e2e
hos8 <jam39so@outlook.com>
2024-07-20 13:50:07 +0800

    Modify education.cpp

--- education.cpp
+++ education.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Best floor cause article enjoy small.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
