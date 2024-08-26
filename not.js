7f13459e6be2d9bb8ee1281ddf0656323eaf85c5
hos8 <jam39so@outlook.com>
2024-08-26 11:13:04 +0800

    Delete not.js

--- not.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Trial rich cold performance cup major strong.
-console.log("Hello, World!");
-
-# Crime watch above service must natural hour.
-console.log("Hello, World!");
