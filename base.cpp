93367b2252fffb01d49af61bd015111d33576d43
hos8 <jam39so@outlook.com>
2024-09-04 13:24:15 +0800

    Modify base.cpp

--- base.cpp
+++ base.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Despite bank successful including training state capital include.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
