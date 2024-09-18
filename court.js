a6e60f8d3b1303873deabeb6c7386be8e39084d3
hos8 <jam39so@outlook.com>
2024-09-18 11:31:43 +0800

    Delete court.js

--- court.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Yeah stage risk among ask effect general.
-console.log("Hello, World!");
-
-# Organization include structure goal themselves easy.
-console.log("Hello, World!");
