37190897c906647a4db63a9356b4997613f23c0c
hos8 <jam39so@outlook.com>
2024-09-21 10:12:12 +0800

    Modify base.cpp

--- base.cpp
+++ base.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Only continue care reflect case activity writer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
