ce28d487987f50a597225f88de759dc56bbc6543
hos8 <jam39so@outlook.com>
2024-09-30 13:21:56 +0800

    Modify challenge.cpp

--- challenge.cpp
+++ challenge.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Threat reason threat describe movement window.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
