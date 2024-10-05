55c0ba1c398c0149b940ec708bc2346c1db64f08
hos8 <jam39so@outlook.com>
2024-10-05 14:14:44 +0800

    Modify present.cpp

--- present.cpp
+++ present.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Less student notice wide air class second.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
