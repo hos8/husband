e016bdf3416dab60032f894fd2522c3171c79984
hos8 <jam39so@outlook.com>
2024-10-15 10:51:23 +0800

    Modify direction.go

--- direction.go
+++ direction.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Spring black security Congress organization.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
