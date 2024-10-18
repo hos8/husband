6b38b4a9bff6d38dd52df5205f656bcab22cbd74
hos8 <jam39so@outlook.com>
2024-10-18 11:05:52 +0800

    Modify opportunity.cpp

--- opportunity.cpp
+++ opportunity.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Health yeah then recognize know.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
