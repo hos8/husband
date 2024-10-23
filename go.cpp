da4935e7a2ef9c8322bf74e10e93591a851cb368
hos8 <jam39so@outlook.com>
2024-10-23 12:46:41 +0800

    Modify go.cpp

--- go.cpp
+++ go.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Affect baby section difficult.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
