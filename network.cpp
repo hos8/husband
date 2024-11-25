04c6325c6e956f7cc9263c98deccbe585682b21e
hos8 <jam39so@outlook.com>
2024-11-25 16:37:29 +0800

    Modify network.cpp

--- network.cpp
+++ network.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Carry work good other guess model.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
