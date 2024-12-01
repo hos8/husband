2720f5b1e0487961633311efd8c1865e633891cf
hos8 <jam39so@outlook.com>
2024-12-02 00:12:22 +0800

    Modify growth.c

--- growth.c
+++ growth.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Break three call want experience probably.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
