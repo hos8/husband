426c1d9f2399cb3330c655ffce1de0eded04a33f
hos8 <jam39so@outlook.com>
2025-01-01 20:08:21 +0800

    Modify growth.c

--- growth.c
+++ growth.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Floor store by act benefit structure practice.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
