ef11e8aa2805df3d1d526efa10f2a78ccfec2304
hos8 <jam39so@outlook.com>
2025-01-15 12:04:32 +0800

    Modify Mr.cpp

--- Mr.cpp
+++ Mr.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Star car magazine add.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
