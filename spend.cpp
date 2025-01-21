cd5173fa18baa7f79a8716930b22837164296fa2
hos8 <jam39so@outlook.com>
2025-01-21 14:14:05 +0800

    Modify spend.cpp

--- spend.cpp
+++ spend.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Think care small big reality.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
