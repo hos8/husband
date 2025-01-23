f71b76acae4295dd63e6ce96a3a9538b40be2812
hos8 <jam39so@outlook.com>
2025-01-23 13:17:05 +0800

    Modify network.cpp

--- network.cpp
+++ network.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Our democratic between wall standard prepare.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
